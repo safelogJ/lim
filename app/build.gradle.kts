75a84607a9ef96f2f0c481cce66dd4f61dc91e47
attl <morebass304@gmail.com>
2026-09-08 18:46:02 +0300

    app-1.17-c15

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ configure<ApplicationExtension> {
         applicationId = "com.safelogj.lim"
         minSdk = 29
         targetSdk = 37
-        versionCode = 16
-        versionName = "app-1.15-c15"
+        versionCode = 17
+        versionName = "app-1.17-c15"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
