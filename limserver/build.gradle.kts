6eb55f313cd3e99ab6eca2615d895bbe35b2e71a
attl <morebass304@gmail.com>
2026-09-23 04:52:04 +0300

    arm32

--- limserver/build.gradle.kts
+++ limserver/build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
 version = "1.18"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 tasks.getByName("shadowJar", {
